c71d7f28f8695e98c1aefd9a5e50421009684fb0
Josh Hofing <joshhofing@gmail.com>
2018-09-11 15:18:42 -0400

    actual reload

--- reload.sh
+++ reload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-make app.sql && (docker exec -i wtf_sql '/bin/bash' <<< 'mysql -uapp_sql -papp_sql') < app.sql
+make app.sql
+(docker exec -i wtf_sql '/bin/bash' '-c' 'mysql -uapp_sql -papp_sql app_sql') < ./app.sql
