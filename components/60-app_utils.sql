ce69560c9e5c123649664af7b5563e82b9100689
Josh Hofing <joshhofing@gmail.com>
2018-09-11 17:10:56 -0400

    post listing

--- components/60-app_utils.sql
+++ components/60-app_utils.sql
@@ -63,8 +63,41 @@ BEGIN
     SET users_table = CONCAT('<table>', stuff, '</table>');
 END$$
 
+DROP PROCEDURE IF EXISTS `create_post`$$
+CREATE PROCEDURE `create_post` (IN `i_user_id` INT, IN `i_text` TEXT)
+BEGIN
+    INSERT INTO `posts` (`user_id`, `text`) VALUES (`i_user_id`, `i_text`);
+END$$
+
+DROP PROCEDURE IF EXISTS `get_user_recent_post_list`$$
+CREATE PROCEDURE `get_user_recent_post_list` (IN `i_user_id` INT, OUT `o_post_list` TEXT)
+BEGIN
+    DECLARE done BOOLEAN;
+    DECLARE curr_row TEXT;
+    DECLARE posts_cur CURSOR FOR SELECT CONCAT('<li><div class=post-text>', `text`, '</div></li>') FROM `posts` WHERE `user_id` = `i_user_id`;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    SET o_post_list = '';
+    OPEN posts_cur;
+    posts_loop: LOOP
+        FETCH posts_cur INTO curr_row;
+        IF done THEN
+            CLOSE posts_cur;
+            LEAVE posts_loop;
+        END IF;
+
+        SET o_post_list = CONCAT(o_post_list, curr_row);
+    END LOOP posts_loop;
+END$$
 
 DELIMITER ;
 
-CALL create_user("foo@foo.com", "foo", "foo_pass");
-CALL create_user("bar@bar.com", "bar", "bar_pass");
+
+-- create some test data:
+
+CALL create_user('foo@foo.com', 'foo', 'foo_pass');
+CALL create_user('bar@bar.com', 'bar', 'bar_pass');
+
+CALL create_post(1, 'post 1');
+CALL create_post(1, 'post 2');
+CALL create_post(1, 'post 3');
--- components/90-routes.sql
+++ components/90-routes.sql
@@ -39,6 +39,20 @@ END$$
 DROP PROCEDURE IF EXISTS `logged_in_index_handler`$$
 CREATE PROCEDURE `logged_in_index_handler` (OUT `status` INT, OUT `resp` TEXT)
 BEGIN
+    DECLARE u_email TEXT;
+    DECLARE user_id INT;
+    DECLARE user_name TEXT;
+    DECLARE post_list TEXT;
+    DECLARE u_id INT;
+
+    CALL get_cookie('email', u_email);
+    SET user_id = (SELECT `id` FROM `users` WHERE `email` = u_email);
+    SET user_name = (SELECT `name` FROM `users` WHERE `email` = u_email);
+
+    CALL get_user_recent_post_list(user_id, post_list);
+    CALL set_template_var('post_list', post_list);
+    CALL set_template_var('user_name', user_name);
+    
     SET status = 200;
     CALL template('/templates/index_logged_in.html', resp);
 END$$
