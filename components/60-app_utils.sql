1e4b71443060b8d7a25bed4eb98fcb97ab5f28a9
Nick Gregory <computerfreak97@gmail.com>
2018-09-14 02:21:15 -0400

    remove last testing data

--- components/60-app_utils.sql
+++ components/60-app_utils.sql
@@ -106,26 +106,20 @@ BEGIN
         SET tbl_name = i_table_name;
     END IF;
 
+    SET cols = NULL;
     SET cols = (SELECT GROUP_CONCAT(column_name) FROM information_schema.columns WHERE `table_schema` = `db_name` AND `table_name` = `tbl_name`);
-    SET cols = COALESCE(cols, '\'\'');
 
-    SET @dump_query = (SELECT CONCAT('SELECT CONCAT(\'<tr>\', GROUP_CONCAT(CONCAT(\'<td>\', CONCAT_WS(\'</td><td>\', ', cols, '), \'</td>\') SEPARATOR \'</tr><tr>\'), \'</tr>\') INTO @dump_result FROM ', `i_table_name`, ';'));
-    PREPARE prepped_query FROM @dump_query;
-    EXECUTE prepped_query;
+    IF ISNULL(cols) THEN
+        SET o_html = 'No such table';
+    ELSE
+        SET @dump_query = (SELECT CONCAT('SELECT CONCAT(\'<tr>\', GROUP_CONCAT(CONCAT(\'<td>\', CONCAT_WS(\'</td><td>\', ', cols, '), \'</td>\') SEPARATOR \'</tr><tr>\'), \'</tr>\') INTO @dump_result FROM ', `i_table_name`, ';'));
+        PREPARE prepped_query FROM @dump_query;
+        EXECUTE prepped_query;
 
-    SET cols = (SELECT CONCAT('<tr><td>', GROUP_CONCAT(column_name SEPARATOR '</td><td>'), '</td></tr>') FROM information_schema.columns WHERE `table_schema` = `db_name` AND `table_name` = `tbl_name`);
-    SET o_html = CONCAT('<table>', cols, @dump_result, '</table>');
+        SET cols = (SELECT CONCAT('<tr><td>', GROUP_CONCAT(column_name SEPARATOR '</td><td>'), '</td></tr>') FROM information_schema.columns WHERE `table_schema` = `db_name` AND `table_name` = `tbl_name`);
+        SET o_html = CONCAT('<table>', cols, @dump_result, '</table>');
+    END IF;
 END$$
 
 
 DELIMITER ;
-
-
--- create some test data:
-
-CALL create_user('foo@foo.com', 'foo', 'foo_pass');
-CALL create_user('bar@bar.com', 'bar', 'bar_pass');
-
-CALL create_post(1, 'post 1');
-CALL create_post(1, 'post 2');
-CALL create_post(1, 'post 3');
